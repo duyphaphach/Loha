7cbd94b9d11dcae09a45f801a0e826b69563c601
duyphaphach <duyphamle97@gmail.com>
2019-09-19 15:28:00 +0700

    WEB-6, WEB-7
    WEB-6 Implement left sidebar menu
    WEB-7 Implement promotion section

--- source/scripts/pages/home.js
+++ source/scripts/pages/home.js
@@ -1,9 +1,43 @@
 module.exports = function (sandbox) {
   const _this = this;
   _this.bindEvents = () => {
+    // Next slide button event
+    _this.objects.$nextButton.on('click', (event) => {
+      event.preventDefault();
+      _this.nextSlide();
+    });
+
+    // Previous slide button event
+    _this.objects.$backButton.on('click', (event) => {
+      event.preventDefault();
+      _this.backSlide();
+    });
+  };
+
+  _this.renderSection1Slider = () => {
+    _this.objects.$section1Slider.slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      swipe: true,
+      cssEase: 'ease-out',
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      appendDots: _this.objects.$section1Slider,
+    });
+  };
+
+  _this.nextSlide = () => {
+    _this.objects.$section1Slider.slick('slickNext');
+  };
+
+  _this.backSlide = () => {
+    _this.objects.$section1Slider.slick('slickPrev');
   };
 
   _this.render = () => {
+    _this.renderSection1Slider();
   };
 
   return {
@@ -12,6 +46,9 @@ module.exports = function (sandbox) {
 
       _this.DOMSelectors = {};
       _this.objects = {
+        $section1Slider: $('#section_1__content__group_1__slider'),
+        $nextButton: $('#section_1__content__group_1 .next'),
+        $backButton: $('#section_1__content__group_1 .back'),
       };
       _this.templates = {};
 
