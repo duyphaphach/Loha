bd4a503ab7e5a411c0b1284bf8bbff9c898a7a4e
duyphaphach <duyphamle97@gmail.com>
2019-09-20 17:36:13 +0700

    WEB-10 Implement Danh sách sản phẩm theo category

--- source/scripts/pages/home.js
+++ source/scripts/pages/home.js
@@ -1,17 +1,6 @@
 module.exports = function (sandbox) {
   const _this = this;
   _this.bindEvents = () => {
-    // Next slide button event
-    _this.objects.$nextButton.on('click', (event) => {
-      event.preventDefault();
-      _this.nextSlide();
-    });
-
-    // Previous slide button event
-    _this.objects.$backButton.on('click', (event) => {
-      event.preventDefault();
-      _this.backSlide();
-    });
   };
 
   _this.renderSection1Slider = () => {
@@ -28,14 +17,6 @@ module.exports = function (sandbox) {
     });
   };
 
-  _this.nextSlide = () => {
-    _this.objects.$section1Slider.slick('slickNext');
-  };
-
-  _this.backSlide = () => {
-    _this.objects.$section1Slider.slick('slickPrev');
-  };
-
   _this.render = () => {
     _this.renderSection1Slider();
   };
@@ -47,8 +28,6 @@ module.exports = function (sandbox) {
       _this.DOMSelectors = {};
       _this.objects = {
         $section1Slider: $('#section_1__content__group_1__slider'),
-        $nextButton: $('#section_1__content__group_1 .next'),
-        $backButton: $('#section_1__content__group_1 .back'),
       };
       _this.templates = {};
 
