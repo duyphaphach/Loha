ed70d35aa898da2488e93d644267ceba9482ad99
huyhoquang <huy@passcareer.io>
2019-09-22 00:08:06 +0700

    Implement banner

--- source/scripts/modules/filter-panel-pricing.js
+++ source/scripts/modules/filter-panel-pricing.js
@@ -7,7 +7,7 @@ module.exports = function (sandbox) {
   return {
     init: (data = {}) => {
       _this.data = data;
-      $('#main__section_1__row__sidebar_menu__filter_panel_3__range_slider__input').ionRangeSlider({
+      $('#main__row__sidebar_menu__filter_panel_3__range_slider__input').ionRangeSlider({
         skin: 'round',
         type: 'double',
         min: 50000,
