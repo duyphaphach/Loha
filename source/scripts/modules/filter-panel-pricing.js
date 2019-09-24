e8e1955150382cdc65813c2465747f612d15ff5a
huyhoquang <huy@passcareer.io>
2019-09-24 12:02:13 +0700

    WEB-17 Move product list to module

--- source/scripts/modules/filter-panel-pricing.js
+++ source/scripts/modules/filter-panel-pricing.js
@@ -7,7 +7,7 @@ module.exports = function (sandbox) {
   return {
     init: (data = {}) => {
       _this.data = data;
-      $('#main__row__sidebar_menu__filter_panel_3__range_slider__input').ionRangeSlider({
+      $('#filter_panel__panel_info_3__range_slider__input').ionRangeSlider({
         skin: 'round',
         type: 'double',
         min: 50000,
